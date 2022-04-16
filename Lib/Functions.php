0892e4ec1aa1b0ccc3756c4064a736ec81a5c7d1
anoopsingam <anooprathod7@gmail.com>
2022-04-17 00:54:55 +0530

    added ay & section functions

--- Lib/Functions.php
+++ Lib/Functions.php
@@ -2,9 +2,19 @@
 
 class func{
 
+    /**
+     * @param string url route
+     * returns the project server url with route
+     */
     static function href($page){
         echo ($page==null)?url::myurl()."#":url::myurl().$page;
     }
+
+    /**
+     * Function to fetch Class list
+     * @param string form:name 
+     * @param array class_list
+     */
     static function classlist(String $name='present_class', array $args=array()){
             echo "<select class='form-control m-1' name='".$name."'>\n";
             echo"<option value=''>Select Class </option>\n";
@@ -26,6 +36,38 @@ class func{
             echo"<option value='9'>9 Std</option>\n";
             echo"<option value='10'>10 Std</option>\n";
             echo "</select>";
+    }
+    static function academicYear(string $ay="ay"){
+        echo"<select class='form-control m-1' name='$ay'>\n";
+        echo"<option value=''>Select Academic Year </option>\n";
+        echo"<option value='2020-2021'>2020-2021</option>\n";
+        echo"<option value='2021-2022'>2021-2022</option>\n";
+        echo"<option value='2022-2023'>2022-2023</option>\n";
+        echo"<option value='2023-2024'>2023-2024</option>\n";
+        echo"<option value='2024-2025'>2024-2025</option>\n";
+        echo"<option value='2025-2026'>2025-2026</option>\n";
+        echo"<option value='2026-2027'>2026-2027</option>\n";
+        echo"<option value='2027-2028'>2027-2028</option>\n";
+        echo"<option value='2028-2029'>2028-2029</option>\n";
+        echo"<option value='2029-2030'>2029-2030</option>\n";
+        echo"</select>";
+    }
+    static function sectionList(){
+        echo "<select class='form-control m-1' name='$section'>\n";
+        echo"<option value=''>Select Section </option>\n";
+        echo"<option value='A'>A</option>\n";
+        echo"<option value='B'>B</option>\n";
+        echo"<option value='C'>C</option>\n";
+        echo"<option value='D'>D</option>\n";
+        echo"<option value='E'>E</option>\n";
+        echo"<option value='F'>F</option>\n";
+        echo"<option value='G'>G</option>\n";
+        echo"<option value='H'>H</option>\n";
+        echo"<option value='I'>I</option>\n";
+        echo"<option value='J'>J</option>\n";
+        echo"<option value='K'>K</option>\n";
+        echo"<option value='L'>L</option>\n";
+        echo"</select>";
     }
 
 }
--- views/admission/QuickAdmission.php
+++ views/admission/QuickAdmission.php
@@ -8,7 +8,7 @@ $app->setTitle("Quick Admission");
     </div>
     <div class="card-body">
         <form action="<?=func::href('/QuickAdmissionController/new')?>" method="post">
-        <h3 class="text-dark m-3">Student Details </h3>
+            <h3 class="text-dark m-3">Student Details </h3>
             <div class="row">
                 <div class="col-md-4">
                     <label for="student_name">Student Name</label><input class="form-control m-1" type="text"
@@ -26,19 +26,57 @@ $app->setTitle("Quick Admission");
                     <label for="dob">DOB</label><input class="form-control m-1" type="date" name="dob" id="dob" />
                 </div>
                 <h3 class="text-dark m-3">Parents Details </h3>
-                <div class="row">
-                <div class="col-md-3">
-                            <label for="father_name">Father Name</label><input class="form-control m-1" type="text"
-                                name="father_name" id="father_name" />
+                <div class="row mb-1">
+                    <div class="col-md-6">
+                        <label for="father_name">Father Name</label><input class="form-control m-1" type="text"
+                            name="father_name" id="father_name" />
+                    </div>
+                    <div class="col-md-6">
+                        <label for="father_number">Father Number</label><input class="form-control m-1" type="number"
+                            name="father_number" id="father_number" />
+                    </div>
+                </div>
+                <div class="row mb-1">
+                <div class="col-md-6">
+                        <label for="mother_name">Mother Name</label><input class="form-control m-1" type="text"
+                            name="mother_name" id="mother_name" />
+                    </div>
+                    <div class="col-md-6">
+                            <label for="mother_number">Mother Number</label><input class="form-control m-1"
+                                type="number" name="mother_number" id="mother_number" />
                         </div>
-                        <div class="col-md-4">
-                            <label for="father_number">Father Number</label><input class="form-control m-1"
-                                type="number" name="father_number" id="father_number" />
-                        </div>
-                        <div class="col-md-4">
-                            <label for="mother_name">Mother Name</label><input class="form-control m-1" type="text"
-                                name="mother_name" id="mother_name" />
-                        </div>
+                </div>
+                <h3 class="text-dark m-3">Residence Details </h3>
+                <div class="row mb-1">
+                    <div class="col-md-6">
+                    <label for="permanentaddress">Permanent Address</label>
+                            <textarea name="permanentaddress" id="permanentaddress" cols="30" rows="5"
+                                class="form-control m-1"></textarea>
+                    </div>
+                    <div class="col-md-6">
+                    <label for="temporaryaddress">Temporary Address</label>
+                            <textarea name="temporaryaddress" id="temporaryaddress" cols="30" rows="5"
+                                class="form-control m-1"></textarea>
+                    </div>
+                </div>
+                <h3 class="text-dark m-3">Academic Details </h3>
+                <div class="row mb-1">
+                    <div class="col-sm">
+                            <label for="medium_c">Medium</label>
+                            <select name="medium_c" class="form-control m-1" id="">
+                                <option value="">Select Medium</option>
+                                <option value="ENGLISH">ENGLISH</option>
+                                <option value="KANNADA">KANNADA</option>
+                                <option value="HINDI">HINDI</option>
+                                <option value="URDU">URDU</option>
+                                <option value="TAMIL">TAMIL</option>.0
+                                <option value="TELUGU">TELUGU</option>
+                                <option value="OTHERS">OTHERS</option>
+                            </select>
+                    </div>
+                    <div class="col-sm"></div>
+                    <div class="col-sm"></div>
+                    <div class="col-sm"></div>
                 </div>
             </div>
         </form>
